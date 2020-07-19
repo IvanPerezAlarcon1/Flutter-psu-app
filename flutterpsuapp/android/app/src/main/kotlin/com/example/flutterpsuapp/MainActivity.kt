c82684050819942447a8dbff1506ef14f5e8e14e
ivan <ivan.perez.alarcon@utem.cl>
2020-07-18 23:39:10 -0400

    Se realizó la pantalla de menú principal una posible versión final

---
+++ flutterpsuapp/android/app/src/main/kotlin/com/example/flutterpsuapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutterpsuapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutterpsuapp/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutterpsuapp/pages/login_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner:
+          false, //para esconder el banner de debug de la esquina superior derecha
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
