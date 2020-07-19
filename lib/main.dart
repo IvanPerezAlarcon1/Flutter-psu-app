210dd1b5bf7804df89923e2e68522fff5fc72f7c
ivan <ivan.perez.alarcon@utem.cl>
2020-07-19 19:40:43 -0400

    Se cambia el nombre del widget de loginpage a nemuprincipal

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutterpsuapp/pages/login_page.dart';
+import 'package:flutterpsuapp/pages/menu_principal.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: LoginPage(),
+      home: MenuPrincipal(),
     );
   }
 }
