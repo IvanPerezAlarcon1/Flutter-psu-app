bbd319e8254db54cd2b708bbfa6da2f16d9809be
ivan <ivan.perez.alarcon@utem.cl>
2020-07-21 01:28:06 -0400

    Se agrega la clase rotacion.dart, se usa para bloquear la rotacion del dispositivo

---
+++ lib/utils/rotacion.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+
+//PARA MÁS INFORMACIÓN DE ESTE CÓDIGO VISITAR https://stackoverflow.com/questions/50322054/flutter-how-to-set-and-lock-screen-orientation-on-demand#:~:text=To%20block%20rotation%20in%20the,build(BuildContext%20context)%20method.&text=To%20block%20rotation%20in%20a,in%20the%20specific%20screen's%20state.
+
+/// Forces portrait-only mode application-wide
+/// Use this Mixin on the main app widget i.e. app.dart
+/// Flutter's 'App' has to extend Stateless widget.
+///
+/// Call `super.build(context)` in the main build() method
+/// to enable portrait only mode
+mixin PortraitModeMixin on StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    _portraitModeOnly();
+    return null;
+  }
+}
+
+/// Forces portrait-only mode on a specific screen
+/// Use this Mixin in the specific screen you want to
+/// block to portrait only mode.
+///
+/// Call `super.build(context)` in the State's build() method
+/// and `super.dispose();` in the State's dispose() method
+mixin PortraitStatefulModeMixin<T extends StatefulWidget> on State<T> {
+  @override
+  Widget build(BuildContext context) {
+    _portraitModeOnly();
+    return null;
+  }
+
+  @override
+  // ignore: must_call_super
+  void dispose() {
+    _enableRotation();
+  }
+}
+
+/// blocks rotation; sets orientation to: portrait
+void _portraitModeOnly() {
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+}
+
+void _enableRotation() {
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+  ]);
+}
